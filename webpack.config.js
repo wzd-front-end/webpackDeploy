f72ab93ae18ee56f7ae93dc378737dced1a57af1
wuzd <429023964@qq.com>
2019-05-21 20:39:01 +0800

    增加测试用例

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: '/\.jpg$/',
+                test: /\.jpg$/,
                 use: {
                     loader: "file-loader"
                 }
